9c35b750facdc603e2dad2dfaa3c7404e45c090a
reijjo <teemu.aitomeri@gmail.com>
2024-07-16 15:44:44 +0300

    mongoose stuff

---
+++ react/09_mongoose_test/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const mongo_url = process.env.MONGODB_URL;
+const mongo_user = process.env.MONGODB_USER;
+const mongo_password = process.env.MONGODB_PASSWORD;
+
+const url =
+  "mongodb+srv://" + mongo_user + ":" + mongo_password + "@" + mongo_url;
+
+mongoose.connect(url).then(
+  () => console.log("Connected to Mongo Atlas."),
+  (err) => console.log("Failed to connect Mongo Atlas.", err)
+);
+console.log("mongo_url", url);
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
