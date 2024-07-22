74fcb611ac671ad517180e20ad8acc29b68cb609
reijjo <teemu.aitomeri@gmail.com>
2024-07-22 20:54:16 +0300

    small

--- react/09_mongoose_test/server.js
+++ react/09_mongoose_test/server.js
@@ -8,7 +8,13 @@ const mongo_user = process.env.MONGODB_USER;
 const mongo_password = process.env.MONGODB_PASSWORD;
 
 const url =
-  "mongodb+srv://" + mongo_user + ":" + mongo_password + "@" + mongo_url;
+  "mongodb+srv://" +
+  mongo_user +
+  ":" +
+  mongo_password +
+  "@" +
+  mongo_url +
+  "?retryWrites=true&w=majority&appName=opiframejutut";
 
 mongoose.connect(url).then(
   () => console.log("Connected to Mongo Atlas."),
